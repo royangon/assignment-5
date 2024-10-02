1480e31ce2999abc746ba3785c8e47399531c91c
royangon <royangon29@gmail.com>
2024-10-02 13:19:33 +0600

    2nd commit

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ document.getElementById("flood-donate-noakhali").addEventListener("click", funct
 
     if (!isNaN(donation) && donation > 0) {
         const reserveMoney = getTextFieldValueById('reserve-field');
-        console.log(reserveMoney)
+       
 
         const updatedReserve = reserveMoney + donation
 
@@ -31,14 +31,14 @@ document.getElementById("flood-donate-noakhali").addEventListener("click", funct
 document.getElementById("flood-donate-feni").addEventListener("click", function (event) {
     event.preventDefault()
     const donation2 = getInputFieldValueByID("money2");
-    console.log(donation2)
+    
 
     if (!isNaN(donation2) && donation2 > 0) {
         const reserveMoney2 = getTextFieldValueById("reserve-field-2");
 
-        console.log(reserveMoney2)
+        
         const updatedReserve2 = reserveMoney2 + donation2
-        console.log(updatedReserve2)
+       
         document.getElementById("reserve-field-2").innerText = updatedReserve2
     }
 
@@ -52,14 +52,14 @@ document.getElementById("flood-donate-feni").addEventListener("click", function 
 document.getElementById("Quota").addEventListener("click", function (event) {
     event.preventDefault()
     const donation3 = getInputFieldValueByID("money3");
-    console.log(donation3)
+   
 
     if (!isNaN(donation3) && donation3 > 0) {
         const reserveMoney3 = getTextFieldValueById("reserve-field-3");
 
-        console.log(reserveMoney3)
+        
         const updatedReserve3 = reserveMoney3 + donation3
-        console.log(updatedReserve3)
+       
         document.getElementById("reserve-field-3").innerText = updatedReserve3
     }
 
