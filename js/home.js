e55fcee1dea580b634f819bf3784fd92612401f9
royangon <royangon29@gmail.com>
2024-10-02 13:50:25 +0600

    2nd commit

--- js/home.js
+++ js/home.js
@@ -5,11 +5,22 @@ document.getElementById("flood-donate-noakhali").addEventListener("click", funct
 
     if (!isNaN(donation) && donation > 0) {
         const reserveMoney = getTextFieldValueById('reserve-field');
-       
+        const myWallet = getTextFieldValueById('wallet')
+        console.log(myWallet)
+
 
         const updatedReserve = reserveMoney + donation
+        const myBallance = myWallet - donation
+        if (myBallance < 0) {
+            alert("not enough money in your AC")
+            return
+        }
+        else {
+            document.getElementById("wallet").innerText = myBallance
+            document.getElementById("reserve-field").innerText = updatedReserve
+        }
 
-        document.getElementById("reserve-field").innerText = updatedReserve
+
 
 
 
@@ -31,14 +42,14 @@ document.getElementById("flood-donate-noakhali").addEventListener("click", funct
 document.getElementById("flood-donate-feni").addEventListener("click", function (event) {
     event.preventDefault()
     const donation2 = getInputFieldValueByID("money2");
-    
+
 
     if (!isNaN(donation2) && donation2 > 0) {
         const reserveMoney2 = getTextFieldValueById("reserve-field-2");
 
-        
+
         const updatedReserve2 = reserveMoney2 + donation2
-       
+
         document.getElementById("reserve-field-2").innerText = updatedReserve2
     }
 
@@ -52,14 +63,14 @@ document.getElementById("flood-donate-feni").addEventListener("click", function 
 document.getElementById("Quota").addEventListener("click", function (event) {
     event.preventDefault()
     const donation3 = getInputFieldValueByID("money3");
-   
+
 
     if (!isNaN(donation3) && donation3 > 0) {
         const reserveMoney3 = getTextFieldValueById("reserve-field-3");
 
-        
+
         const updatedReserve3 = reserveMoney3 + donation3
-       
+
         document.getElementById("reserve-field-3").innerText = updatedReserve3
     }
 
